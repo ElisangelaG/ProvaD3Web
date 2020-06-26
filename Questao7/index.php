49ca9232d795f04a9a9056a8097712941d88d2dd
ElisangelaG <elisangelacristinne@gmail.com>
2020-06-25 22:30:08 -0300

    Prova Final

---
+++ Questao7/index.php
@@ -0,0 +1,19 @@
+<?php
+include_once './SuperHeroi.php';
+$Herois = array(
+    new SuperHeroi(01, "Homem de Ferro", "Voa", 151515, 111111  "Laboratorio"),
+    new SuperHeroi(02, "Thor", "Raio", 111000, 55555, "Venus"),
+    new SuperHeroi(03, "Mulher Maravilha", "Chicote", 151718, 50, "Terra"),
+    new SuperHeroi(04, "Hulk", "Forca", 8000, 50, "Terra"),
+);
+for ($i = 0; $i < count($Herois); $i++) {
+    echo "Id: " . $Herois[$i]->getId() ;    
+    echo "Nome: " . $Herois[$i]->getNome() ;
+    echo "Poder Especial: " . $Herois[$i]->getPoderEspecial() ;
+    echo "Energia: " . $Herois[$i]->getEnergia() ;
+    echo "Forca: " . $Herois[$i]->getForca() ;
+    echo "Origem: " . $Herois[$i]->getOrigem() ;
+    echo "Poder Médio: " . $Herois[$i]->calcularPoderMedio() ;
+    echo "Energia Aumentada: " . $Herois[$i]->aumentarEnergia();
+
+}
